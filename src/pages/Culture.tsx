492dbedac3433a62929eda257b2c160e89ac2d21
Mykhailo Dorokhov <mykhailo.dorokhov@pipedrive.com>
2025-07-10 02:46:59 +0300

    first progress with video embedding

--- src/pages/Culture.tsx
+++ src/pages/Culture.tsx
@@ -18,7 +18,21 @@ function Culture() {
             <div className="content-row">
                 <h3>🎨 Art</h3>
                 <div className="content-row">
-                    <p>Coming soon...</p>
+                    <div className="column">
+                        <iframe 
+                            width="360" height="203"
+                            src="https://www.youtube.com/embed/xOcn6hFVdDo?si=rLkcqaNbNGy3KuLu?controls=0&autoplay=1&modestbranding=1&rel=0" 
+                            title="2:00AM bt Tony Skeor"
+                            frameBorder="0"
+                            allow="autoplay; encrypted-media; web-share; " 
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen></iframe>
+                    </div>
+                    <div className="column">
+                        <p> 2:00 AM | TΞMPLUM by <a href="https://www.artstation.com/skeor" target="_blank" rel="noopener noreferrer">Tony Skeor</a></p>
+                        <p>Tony is my absolute favourite artist. Not only is he extremely talented, but he is also a huge inspiration that clicks with me: his dreamy, yet not dystopin future.</p>
+                        <p>Check <a href="https://www.artstation.com/artwork/d16Re" target="_blank" rel="noopener noreferrer">2:00 AM</a>, <a href="https://www.artstation.com/artwork/8e8aX6" target="_blank" rel="noopener noreferrer">Intro</a> and <a href="https://www.artstation.com/artwork/6LNDZr" target="_blank" rel="noopener noreferrer">Loners</a> from the TΞMPLUM series, as well as his other art on his <a href="https://www.artstation.com/skeor" target="_blank" rel="noopener noreferrer">Artstation</a>.</p>
+                    </div>
                 </div>
             </div>
 
