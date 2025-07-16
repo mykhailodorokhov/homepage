fb7bada3c85f705d9b956578cab06e5c6b940c84
Mykhailo Dorokhov <mykhailo.dorokhov@pipedrive.com>
2025-07-16 04:16:05 +0300

    Added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Culture from './pages/Culture';
 import CV from './pages/CV';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/cv" element={<CV />} />
           <Route path="/culture" element={<Culture />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+function NotFound() {
+  return (
+    <main>
+      <div className="title-block content-row">
+          <div>
+              <h1>404</h1>
+              <h2>Looks like you've wandered off the trail</h2>
+          </div>
+      </div>
+    </main>
+  );
+}
+
+export default NotFound;
